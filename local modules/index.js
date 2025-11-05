8db190ed7a24c98eeca8f60a2d77b647deb91206
Madhu Sudhan <madhusudhanrao391@gmail.com>
2025-11-05 21:00:50 +0530

    local modules

---
+++ local modules/index.js
@@ -0,0 +1,15 @@
+
+// this is a local module
+require('./add.js');
+
+// In node.js, each file is a module that is isolated by default
+
+// To load a module into another file, we use the require function
+
+// When index.js is executed, the code in the module is also executed
+
+// If the file we are requiring is a javascript file, we can skip specifying the extension
+// and node.js will infer it on our behalf
+
+
+console.log('hello world');
