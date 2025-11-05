17fb2827e4a83e10e440d04301657db8e62ce0ee
Madhu Sudhan <madhusudhanrao391@gmail.com>
2025-11-05 21:57:16 +0530

    json and watch mode

---
+++ json/index.js
@@ -0,0 +1,9 @@
+const data = require('./data.json');
+
+
+// watch mode -> node --watch filename
+// using watch name we need not run the program again and again
+
+console.log(data);
+console.log(data.address);
+
